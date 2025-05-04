841bceb74a32584418dfce118fdbd99b32695074
Francois LaBerge <fgrclaberge@gmail.com>
2025-05-04 01:32:07 -0400

    center clocks

--- src/main.rs
+++ src/main.rs
@@ -2,95 +2,107 @@ use core::f64;
 use std::{io, time::Duration};
 
 use chrono::{DateTime, Local, TimeZone, Timelike, Utc};
-use crossbeam_channel::{select, tick, unbounded, Receiver};
-use crossterm::{event::DisableMouseCapture, execute, terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}};
-use tui::{backend::CrosstermBackend, style::{Color, Style}, text::{Span, Spans}, widgets::{Block, Borders, Paragraph, Wrap}, Terminal};
+use crossterm::event::{self, Event};
+use ratatui::layout::{Constraint, Direction, Flex, Layout};
+use ratatui::Frame;
+use ratatui::{layout::Alignment, style::{Color, Style}, text::{Line, Text}, widgets::{Block, Borders, Paragraph, Wrap}};
 
 const FG_COLOR: Color = Color::Rgb(167, 199, 231);
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 struct Position {
     lon: f64,
+    lat: f64,
+    city: String,
 }
 
 fn main() -> Result<(), io::Error> {
-    let mut terminal = setup_terminal()?;
-    let ctrl_c_events = ctrl_channel().unwrap();
-    let ticks = tick(Duration::from_secs(1));
+    let mut terminal = ratatui::init();
 
-    let lon = get_longitude();
+
+    let pos = get_position();
     loop {
-        select! {
-            recv(ticks) -> _ => {
-                render(&mut terminal, lon)?;
-            }
-            recv(ctrl_c_events) -> _ => {
+        let _ = terminal.draw(|f| render(f, &pos).unwrap());
+
+        if event::poll(Duration::from_millis(100))?  {
+            if matches!(event::read()?, Event::Key(_)){
                 break;
             }
-        };
+
+        }
     };
-    let _ = cleanup_terminal::<CrosstermBackend<io::Stdout>>(&mut terminal);
+    ratatui::restore();
     Ok(())
 }
 
-fn render(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>, lon: f64) -> Result<(), io::Error>{
+fn render(f: &mut Frame, pos: &Position) -> Result<(), io::Error>{
+    let title = format!(" {}[{}, {}] ", pos.city, pos.lat, pos.lon);
     let utc = Utc::now();
     let now = Local::now();
-    let geograhic = get_geographic_time(utc, lon);
-    let utc_s = format_clock(utc)?;
-    let now_s = format_clock(now)?;
-    let geographic_s = format_clock(geograhic)?;
+    let geograhic = get_geographic_time(utc, pos.lon);
 
-    let utc_label = "UTC";
-    let local_label = "Local";
-    let geographic_label = "Geographic";
-    let _ = terminal.draw(|f| {
-        let size = f.size();
-        let clock_text = vec![
-            Spans::from(vec![
-                Span::raw("UTC: "),
-                Span::raw(utc_s),
-            ]),
-            Spans::from(vec![
-                Span::raw("Loc: "),
-                Span::raw(now_s),
-            ]),
-            Spans::from(vec![
-                Span::raw("Geo: "),
-                Span::raw(geographic_s),
-            ]),
-        ];
-        let vertical_padding = size.height / 2 - clock_text.len() as u16;
-        let mut text: Vec<Spans> = vec![""; vertical_padding as usize]
-            .into_iter()
-            .map(|e| Spans::from(vec![Span::raw(e)]))
-            .collect();
-        text.extend(clock_text.iter().cloned());
+    let lines = vec![
+        format!("{} {}", format_clock(utc)?, "UTC"),
+        format!("{} {}", format_clock(now)?, "Local"),
+        format!("{} {}", format_clock(geograhic)?, "Geographic"),
+    ];
 
-        let paragraph = Paragraph::new(text)
-            .block(Block::default().borders(Borders::ALL))
-            .style(Style::default().fg(FG_COLOR).bg(Color::Black))
-            .alignment(tui::layout::Alignment::Center)
-            .wrap(Wrap { trim: true });
-        f.render_widget(paragraph, f.size());
+    let clock_text = Text::from(
+        lines.iter().map(|l|
+            Line::from(l.clone())
+        ).collect::<Vec<Line>>()
+    );
+
+    let length = lines.len();
+    let width = lines.iter().fold(0, |a, l| {
+        if l.len() > a {
+            l.len()
+        } else {
+            a
+        }
     });
+
+    let vertical_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Fill(1),
+            Constraint::Length(length as u16),
+            Constraint::Fill(1),
+        ])
+        .flex(Flex::Center)
+        .split(f.area());
+
+    let horizontal_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Fill(1),
+            Constraint::Length(width as u16),
+            Constraint::Fill(1),
+        ])
+        .flex(Flex::Center)
+        .split(vertical_layout[1]);
+
+    let paragraph = Paragraph::new(clock_text)
+        .style(Style::default().fg(FG_COLOR).bg(Color::Black))
+        .alignment(Alignment::Left)
+        .wrap(Wrap { trim: true });
+
+    let block = Block::default()
+        .title(title)
+        .style(Style::default().fg(FG_COLOR).bg(Color::Black))
+        .borders(Borders::ALL);
+
+    f.render_widget(block, f.area());
+    f.render_widget(paragraph, horizontal_layout[1]);
 
     Ok(())
 }
 
-fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
-    let (sender, receiver) = unbounded();
-    ctrlc::set_handler(move || {
-        let _ = sender.send(());
-    })?;
-    Ok(receiver)
-}
-
-fn get_longitude() -> f64 {
+fn get_position() -> Position {
     let res: Position = reqwest::blocking::get("http://ip-api.com/json")
         .unwrap()
         .json()
         .unwrap();
-    return res.lon
+    return res
 }
 
 fn longitude_to_second(lat: f64) -> i64 {
@@ -104,27 +116,6 @@ fn get_geographic_time(utc: DateTime<Utc>, lon: f64) -> DateTime<Utc> {
     } else {
         return utc - Duration::from_secs((-1*offset) as u64)
     };
-}
-
-fn setup_terminal() -> Result<Terminal<CrosstermBackend<io::Stdout>>, io::Error>{
-    enable_raw_mode()?;
-    let stdout = io::stdout();
-    execute!(io::stdout(), EnterAlternateScreen)?;
-    let terminal = Terminal::new(CrosstermBackend::new(stdout))?;
-    disable_raw_mode()?;
-    return Ok(terminal);
-}
-
-fn cleanup_terminal<T>(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> Result<(), io::Error> {
-    // terminal.clear()?;
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-    return Ok(());
 }
 
 fn format_clock<T: TimeZone>(time: DateTime<T>) -> Result<String, io::Error> {
